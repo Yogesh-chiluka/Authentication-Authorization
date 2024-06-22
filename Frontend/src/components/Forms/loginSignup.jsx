802f5650e38b938dc39fe0949428ba2a0d08b149
Yogesh Chiluka <72222615+Yogesh-chiluka@users.noreply.github.com>
2024-06-22 16:37:49 +0000

    Add Layout and Forms - install tailwind css - add layout for login/register/signup froms - add loginSignup form - add register from - add user roles login from - add login form

---
+++ Frontend/src/components/Forms/loginSignup.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+export default function SignUpLogin(){
+
+
+    return(
+
+        <div className="text-xl text-gray-300 p-4 w-80 sm:w-1/4 h-1/4 bg-gray-400 border rounded border-transparent drop-shadow-2xl grid grid-rows-1 p-8">
+           
+            <button className='border rounded border-transparent drop-shadow-2xl bg-gray-900 my-2 py-2 px-4 hover:bg-gray-800 text-gray-200'> Login </button>
+            <button className='border rounded border-transparent drop-shadow-2xl bg-gray-900 my-2 py-2 px-4 hover:bg-gray-800 text-gray-200'> signUP </button>
+            
+        </div>)
+}
---
+++ Frontend/src/components/Layouts/layoutLoginSignup.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Layout(){
+
+    return(<div className="min-h-screen bg-gray-300 flex justify-center items-center ">
+    </div>)
+}
